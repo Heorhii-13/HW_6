86a9be1e7fcb9c743bb90268c860cbb8d2c8e554
Heorhii-13 <heorhii.batyrov@gmail.com>
2025-03-23 17:19:00 +0200

    hw_6

---
+++ HW_6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+// Завдання 1: Клас "П'єса"
+class Play
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public int Year { get; set; }
+
+    public Play(string title, string author, string genre, int year)
+    {
+        Title = title;
+        Author = author;
+        Genre = genre;
+        Year = year;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"П'єса: {Title}, Автор: {Author}, Жанр: {Genre}, Рік: {Year}");
+    }
+
+    ~Play()
+    {
+        Console.WriteLine($"П'єса {Title} знищена");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Тестування класу "П'єса"
+        Play play = new Play("Гамлет", "Вільям Шекспір", "Трагедія", 1601);
+        play.ShowInfo();
+    }
+}
